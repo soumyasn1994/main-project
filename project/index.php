b090ec832affa95911efa1addc8feaeeb602a976
soumyasn1994 <33921622+soumyasn1994@users.noreply.github.com>
2019-06-02 16:06:46 +0530

    Add files via upload

---
+++ project/index.php
@@ -0,0 +1,394 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<!-- Basic Page Needs
+    ================================================== -->
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>Mansion Care</title>
+
+<!-- Favicons
+    ================================================== -->
+<link rel="shortcut icon" href="img/favicon.ico" type="image/x-icon">
+<link rel="apple-touch-icon" href="img/apple-touch-icon.png">
+<link rel="apple-touch-icon" sizes="72x72"
+	href="img/apple-touch-icon-72x72.png">
+<link rel="apple-touch-icon" sizes="114x114"
+	href="img/apple-touch-icon-114x114.png">
+
+<!-- Bootstrap -->
+<link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+<link rel="stylesheet" type="text/css"
+	href="fonts/font-awesome/css/font-awesome.css">
+
+<!-- Slider
+    ================================================== -->
+<link href="css/owl.carousel.css" rel="stylesheet" media="screen">
+<link href="css/owl.theme.css" rel="stylesheet" media="screen">
+
+<!-- Stylesheet
+    ================================================== -->
+<link rel="stylesheet" type="text/css" href="css/style.css">
+<link rel="stylesheet" type="text/css" href="css/responsive.css">
+
+<link
+	href='http://fonts.googleapis.com/css?family=Lato:100,300,400,700,900,100italic,300italic,400italic,700italic,900italic'
+	rel='stylesheet' type='text/css'>
+<link
+	href='http://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,700,300,600,800,400'
+	rel='stylesheet' type='text/css'>
+
+<script type="text/javascript" src="js/modernizr.custom.js"></script>
+
+</head>
+<body>
+	<!-- Navigation
+    ==========================================-->
+	<nav id="tf-menu" class="navbar navbar-default navbar-fixed-top">
+		<div class="container">
+			<!-- Brand and toggle get grouped for better mobile display -->
+			<div class="navbar-header">
+				<button type="button" class="navbar-toggle collapsed"
+					data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+					<span class="sr-only">Toggle navigation</span> <span
+						class="icon-bar"></span> <span class="icon-bar"></span> <span
+						class="icon-bar"></span>
+				</button>
+				<a class="navbar-brand" href="index.php">Mansion Care</a>
+			</div>
+
+			<!-- Collect the nav links, forms, and other content for toggling -->
+			<div class="collapse navbar-collapse"
+				id="bs-example-navbar-collapse-1">
+				<ul class="nav navbar-nav navbar-right">
+					<li><a href="#tf-home" class="page-scroll">Home</a></li>
+					<li><a href="#tf-services" class="page-scroll">Services</a></li>
+					<li><a href="#tf-login" class="page-scroll">Login</a></li>
+					<li><a href="userreg.php" >User Sign Up</a></li>
+					<li><a href="empreg.php" >Staff Sign Up</a></li>
+				</ul>
+			</div>
+			<!-- /.navbar-collapse -->
+		</div>
+		<!-- /.container-fluid -->
+	</nav>
+
+	
+	<!-- Home Page
+    ==========================================-->
+	<div id="tf-home" class="text-center">
+		<div class="overlay">
+			<div class="content">
+				<h1>
+					Welcome To <strong><span class="color">Mansion Care</span></strong>
+				</h1>
+				<p class="lead">
+					We help you with all the services you need for your day at your doorstep on the day you choose.
+				</p>
+				<a href="#tf-about" class="fa fa-angle-down page-scroll"></a>
+			</div>
+		</div>
+	</div>
+
+
+	<!-- Services Section
+    ==========================================-->
+	<div id="tf-services">
+		<div class="container">
+			<!-- Container -->
+			<div class="section-title text-center center">
+				<h2>
+					Take a look at <strong>our policies</strong>
+				</h2>
+				<div class="line">
+					<hr>
+				</div>
+				<div class="clearfix"></div>
+				<small><em>We offer you a wide range of policies each of which are designed to give you maximum cover 
+				and protection.</em></small>
+			</div>
+			<div class="space"></div>
+
+			<div class="categories">
+
+				<ul class="cat">
+					<li class="pull-left"><h4>Filter by Type:</h4></li>
+					<li class="pull-right">
+						<ol class="type">
+							<li><a href="#" data-filter="*" class="active">All</a></li>
+							<li><a href="#" data-filter=".health">Home</a></li>
+							<li><a href="#" data-filter=".pension">Personal Care</a></li>
+							<li><a href="#" data-filter=".family">Labour Work</a></li>
+							
+						</ol>
+					</li>
+				</ul>
+				<div class="clearfix"></div>
+			</div>
+
+			<div id="lightbox" class="row">
+
+				<div class="col-sm-6 col-md-3 col-lg-3 health">
+					<div class="portfolio-item">
+						<div class="hover-bg">
+							<a href="#">
+								<div class="hover-text">
+									<h4>Carpentory</h4>
+									<small>Everything related to wood work</small>
+									<div class="clearfix"></div>
+									<i class="fa fa-plus"></i>
+								</div> <img src="img/portfolio/001.jpg" class="img-responsive"
+								alt="...">
+							</a>
+						</div>
+					</div>
+				</div>
+
+				<div class="col-sm-6 col-md-3 col-lg-3 family health">
+					<div class="portfolio-item">
+						<div class="hover-bg">
+							<a href="#">
+								<div class="hover-text">
+									<h4>Machine Washing</h4>
+									<small>Don't worry of dirty clothes</small>
+									<div class="clearfix"></div>
+									<i class="fa fa-plus"></i>
+								</div> <img src="img/portfolio/002.jpg" class="img-responsive"
+								alt="...">
+							</a>
+						</div>
+					</div>
+				</div>
+
+				<div class="col-sm-6 col-md-3 col-lg-3 pension">
+					<div class="portfolio-item">
+						<div class="hover-bg">
+							<a href="#">
+								<div class="hover-text">
+									<h4>Cleaning</h4>
+									<small>Spotless home</small>
+									<div class="clearfix"></div>
+									<i class="fa fa-plus"></i>
+								</div> <img src="img/portfolio/003.jpg" class="img-responsive"
+								alt="...">
+							</a>
+						</div>
+					</div>
+				</div>
+
+				<div class="col-sm-6 col-md-3 col-lg-3 family">
+					<div class="portfolio-item">
+						<div class="hover-bg">
+							<a href="#">
+								<div class="hover-text">
+									<h4>Painting</h4>
+									<small>Beaultiful walls</small>
+									<div class="clearfix"></div>
+									<i class="fa fa-plus"></i>
+								</div> <img src="img/portfolio/004.jpg" class="img-responsive"
+								alt="...">
+							</a>
+						</div>
+					</div>
+				</div>
+
+				<div class="col-sm-6 col-md-3 col-lg-3 family">
+					<div class="portfolio-item">
+						<div class="hover-bg">
+							<a href="#">
+								<div class="hover-text">
+									<h4>Plumbing</h4>
+									<small>Say no to leaks</small>
+									<div class="clearfix"></div>
+									<i class="fa fa-plus"></i>
+								</div> <img src="img/portfolio/005.jpg" class="img-responsive"
+								alt="...">
+							</a>
+						</div>
+					</div>
+				</div>
+
+				<div class="col-sm-6 col-md-3 col-lg-3 family">
+					<div class="portfolio-item">
+						<div class="hover-bg">
+							<a href="#">
+								<div class="hover-text">
+									<h4>Home Maintanance</h4>
+									<small>Securing your Home</small>
+									<div class="clearfix"></div>
+									<i class="fa fa-plus"></i>
+								</div> <img src="img/portfolio/006.jpg" class="img-responsive"
+								alt="...">
+							</a>
+						</div>
+					</div>
+				</div>
+
+				<div class="col-sm-6 col-md-3 col-lg-3 health">
+					<div class="portfolio-item">
+						<div class="hover-bg">
+							<a href="#">
+								<div class="hover-text">
+									<h4>Repairs</h4>
+									<small>Fix everything broken</small>
+									<div class="clearfix"></div>
+									<i class="fa fa-plus"></i>
+								</div> <img src="img/portfolio/007.jpg" class="img-responsive"
+								alt="...">
+							</a>
+						</div>
+					</div>
+				</div>
+
+				<div class="col-sm-6 col-md-3 col-lg-3 pension">
+					<div class="portfolio-item">
+						<div class="hover-bg">
+							<a href="#">
+								<div class="hover-text">
+									<h4>Renovation</h4>
+									<small>Rebuild from old to new</small>
+									<div class="clearfix"></div>
+									<i class="fa fa-plus"></i>
+								</div> <img src="img/portfolio/008.jpg" class="img-responsive"
+								alt="...">
+							</a>
+						</div>
+					</div>
+				</div>
+
+			</div>
+		</div>
+	</div>
+
+
+	<!-- Contact Section
+    ==========================================-->
+	<div id="tf-contact" class="text-center">
+		<div class="container">
+
+			<div class="row">
+				<div class="col-md-8 col-md-offset-2">
+
+					<div class="section-title center">
+						<h2>
+							Feel free to <strong>contact us</strong>
+						</h2>
+						<div class="line">
+							<hr>
+						</div>
+						<div class="clearfix"></div>
+						<small><em>Flowers or bricks, Feel free to contact us for anything</em></small>
+					</div>
+
+					<form>
+						<div class="row">
+							<div class="col-md-6">
+								<div class="form-group">
+									<label for="exampleInputEmail1">Email address</label> <input
+										type="email" class="form-control" id="exampleInputEmail1"
+										placeholder="Enter email">
+								</div>
+							</div>
+							<div class="col-md-6">
+								<div class="form-group">
+									<label for="exampleInputPassword1">Password</label> <input
+										type="password" class="form-control"
+										id="exampleInputPassword1" placeholder="Password">
+								</div>
+							</div>
+						</div>
+						<div class="form-group">
+							<label for="exampleInputEmail1">Message</label>
+							<textarea class="form-control" rows="3"></textarea>
+						</div>
+
+						<button type="submit" class="btn tf-btn btn-default">Submit</button>
+					</form>
+
+				</div>
+			</div>
+
+		</div>
+	</div>
+
+<!-- Login -->
+
+<div id="tf-login" class="text-center">
+		<div class="overlay">
+			<div class="container">
+				<div class="section-title center">
+					<h2>
+						<strong>Please Login</strong> To book your service
+					</h2>
+					<div class="line">
+						<hr>
+					</div>
+				</div>
+				
+				<div class="container">
+			<div class="row">
+					<div class="col-md-4 col-md-offset-4">
+				<div class="panel panel-success">
+					<div class="panel-heading">
+						<h3 class="panel-title">Please sign in</h3>
+					</div>
+					<div class="panel-body">
+						<form action="data/login.php" method="post">
+							<fieldset>
+								<div class="form-group">
+									<input name="user" type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" <?php if (isset($_SESSION['r_email'])) {echo 'value="' . $_SESSION['r_email'] . '"';} ?> required>
+								</div>
+								<div class="form-group">
+									<input name="pass" type="password" class="form-control" id="exampleInputPassword1" placeholder="Password" required>
+								</div>	
+								<button class="btn btn-lg btn-success btn-block" type="submit" name="submit" value="login">Login</button>
+								<a href="mails/forgotpassword.php" class="a">Forgot Password</a>	
+							</fieldset>
+						</form>
+						</div>
+				</div>
+			</div>
+		</div>
+	</div>
+			</div>
+		</div>
+	</div>
+
+ 
+	<nav id="footer">
+		<div class="container">
+			<div class="pull-left fnav">
+				<p>
+					ALL RIGHTS RESERVED. COPYRIGHT © 2015. 
+				</p>
+			</div>
+			<div class="pull-right fnav">
+				<ul class="footer-social">
+					<li><a href="#"><i class="fa fa-facebook"></i></a></li>
+					<li><a href="#"><i class="fa fa-dribbble"></i></a></li>
+					<li><a href="#"><i class="fa fa-google-plus"></i></a></li>
+					<li><a href="#"><i class="fa fa-twitter"></i></a></li>
+				</ul>
+			</div>
+		</div>
+	</nav>
+
+
+	<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+	<script
+		src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+	<script type="text/javascript" src="js/jquery.1.11.1.js"></script>
+	<!-- Include all compiled plugins (below), or include individual files as needed -->
+	<script type="text/javascript" src="js/bootstrap.js"></script>
+	<script type="text/javascript" src="js/SmoothScroll.js"></script>
+	<script type="text/javascript" src="js/jquery.isotope.js"></script>
+
+	<script src="js/owl.carousel.js"></script>
+
+	<!-- Javascripts
+    ================================================== -->
+	<script type="text/javascript" src="js/main.js"></script>
+
+</body>
+</html>
